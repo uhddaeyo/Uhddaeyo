dd1a566aa3da8047595dacde57a889f8619fbbd7
Hyeonjeong Na <6230nana@gmail.com>
2019-03-05 14:12:15 +0900

    Add Guest sign up and DAO
    고객 회원가입, DAO 추가, mapper.xml 파일 위치 변경

--- src/main/java/com/ktds/uhddaeyo/dao/UserDao.java
+++ src/main/java/com/ktds/uhddaeyo/dao/UserDao.java
@@ -1,12 +1,12 @@
-package com.ktds.uhddaeyo.mapper;
+package com.ktds.uhddaeyo.dao;
 
 import javax.servlet.http.HttpSession;
 
 import com.ktds.uhddaeyo.model.dto.GuestDto;
-import com.ktds.uhddaeyo.model.dto.UserDto;;
+import com.ktds.uhddaeyo.model.dto.UserDto;
 
-public interface UserMapper {
-	public String loginCheck(UserDto user);
+public interface UserDao {
+	public boolean loginCheck(UserDto user, HttpSession session);
 	public UserDto viewMember(UserDto user);
 	public void logout(HttpSession session);
 	public Integer idCheck(String userId);
